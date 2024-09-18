8e24f25f2ac13fe5d05466afeaa694ca62d33a9a
kinjal-nasit-12 <kinjalnasit12@gmail.com>
2024-09-18 09:29:10 -0700

    Update U3P9.java

--- U3P9.java
+++ U3P9.java
@@ -5,64 +5,70 @@ menu driven operations(using methods of String class)
 3.Check for equality with another string
 4.Display substring from a given string */
 import java.util.Scanner;
+
 class U3P9 {
+
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         printMessage("Enter string: ");
         String string = scanner.nextLine();
         int choice;
-        do {
-            printMessage("1.Display string in uppercase.");
-            printMessage("2.Display string in lowercase.");
-            printMessage("3.Check for equality with another string.");
-            printMessage("4.Display substring from a given string.");
-            printMessage("5.Exit.");
-            printMessage("Enter your choice: ");
-            choice = scanner.hasNextInt() ? scanner.nextInt() : 0;
+        printMessage("1.Display string in uppercase.");
+        printMessage("2.Display string in lowercase.");
+        printMessage("3.Check for equality with another string.");
+        printMessage("4.Display substring from a given string.");
+        printMessage("5.Exit.");
+        printMessage("Enter your choice: ");
+        choice = getIntegerValue(scanner);
 
-            switch (choice) {
-                case 1: {
-                    printMessage("String in uppercase: " + string.toUpperCase());
-                    break;
-                }
-                case 2: {
-                    printMessage("String in lowercase: " + string.toLowerCase());
-                    break;
-                }
-                case 3: {
-                    printMessage("Enter another string to compare: ");
-                    String newString = scanner.next();
-                    if (string.equals(newString))
-                        printMessage("String are equal.");
-                    else
-                        printMessage("String are not equal.");
-                    break;
-                }
-                case 4: {
-                    printMessage("Enter starting index of the substring: ");
-                    int startIndex = scanner.hasNextInt() ? scanner.nextInt() : 0;
-                    printMessage("Enter ending index of the substring: ");
-                    int endIndex = scanner.hasNextInt() ? scanner.nextInt() : 0;
-                    if (startIndex >= 0 && endIndex < string.length() && startIndex < endIndex) {
-                        String substring = string.substring(startIndex, endIndex);
-                        printMessage("Substring: " + substring);
-                    } else
-                        printMessage("Invalid starting or ending index.");
-                    break;
-                }
-                case 5: {
-                    printMessage("Exiting the program.");
-                    break;
-                }
-                default:
-                    printMessage("Invalid choice: please enter right choice.");
-                    break;
+        switch (choice) {
+            case 1: {
+                printMessage("String in uppercase: " + string.toUpperCase());
+                break;
             }
-        } while (choice != 5);
+            case 2: {
+                printMessage("String in lowercase: " + string.toLowerCase());
+                break;
+            }
+            case 3: {
+                printMessage("Enter another string to compare: ");
+                String newString = scanner.next();
+                if (string.equals(newString)) {
+                    printMessage("String are equal.");
+                } else {
+                    printMessage("String are not equal.");
+                }
+                break;
+            }
+            case 4: {
+                printMessage("Enter starting index of the substring: ");
+                int startIndex = getIntegerValue(scanner);
+                printMessage("Enter ending index of the substring: ");
+                int endIndex = getIntegerValue(scanner);
+                if (startIndex >= 0 && endIndex < string.length() && startIndex < endIndex) {
+                    String substring = string.substring(startIndex, endIndex);
+                    printMessage("Substring: " + substring);
+                } else {
+                    printMessage("Invalid starting or ending index.");
+                }
+                break;
+            }
+            case 5: {
+                printMessage("Exiting the program.");
+                break;
+            }
+            default:
+                printMessage("Invalid choice: please enter right choice.");
+                break;
+        }
         scanner.close();
     }
 
     public static void printMessage(String message) {
         System.out.println(message);
+    }
+
+    public static int getIntegerValue(Scanner scanner) {
+        return scanner.hasNextInt() ? scanner.nextInt() : 0;
     }
 }
