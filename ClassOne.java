115d24848909985649889bd631c3851f1f99ddab
kinjal-nasit-12 <kinjalnasit12@gmail.com>
2024-09-18 20:17:49 -0700

    Package (U3P2)

---
+++ ClassOne.java
@@ -0,0 +1,12 @@
+//U3P2(Package)
+package Package1;
+public class ClassOne
+{
+    public void display()
+    {
+        printMessage("This is ClassOne.");
+    }
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+}
---
+++ ClassTwo.java
@@ -0,0 +1,12 @@
+//U3P2(Package)
+package Package2;
+public class ClassTwo
+{
+    public void display()
+    {
+        printMessage("This is ClassTwo.");
+    }
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+}
