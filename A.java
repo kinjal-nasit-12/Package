affaa795bc86f0dfe2dc1a1b3ffcdf03b380ce52
kinjal-nasit-12 <kinjalnasit12@gmail.com>
2024-09-18 19:57:25 -0700

    Package file(U3P3)

---
+++ A.java
@@ -0,0 +1,15 @@
+//U3P3(Package)
+package Pack1;
+
+public class A {
+
+    public int x = 10;
+
+    public void show() {
+        printMessage("value of x: " + x);
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+}
