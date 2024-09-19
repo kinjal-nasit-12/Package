5cbfee0fce45e6cf4877bcd0a37bab51315e740d
kinjal-nasit-12 <kinjalnasit12@gmail.com>
2024-09-18 20:00:17 -0700

    Package file(U3P3)

---
+++ ClassThree.java
@@ -0,0 +1,12 @@
+//U3P2(Package)
+package Package3;
+public class ClassThree
+{
+    public void display()
+    {
+        printMessage("This is ClassThree.");
+    }
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+}
