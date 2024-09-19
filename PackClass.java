3334a6f49d71abb7f5e82237803065adc3cebeae
kinjal-nasit-12 <kinjalnasit12@gmail.com>
2024-09-18 19:55:15 -0700

    Package file(U3P1)

---
+++ PackClass.java
@@ -0,0 +1,12 @@
+package P;
+public class PackClass {
+    public int findMax(int a, int b, int c) {
+        if (a > b && a > c)
+            return a;
+        else if (b > c)
+            return b;
+        else
+            return c;
+    }
+}
+
