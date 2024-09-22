fb3ada7554edb2ca4afcc390b9963596e4880790
kinjal-nasit-12 <kinjalnasit12@gmail.com>
2024-09-21 21:53:44 -0700

    Update SumClass.java

--- SumClass.java
+++ SumClass.java
@@ -3,22 +3,14 @@ package SumPackage;
 
 public class SumClass {
 
-    int a;
-    int b;
+    int a, b;
 
-    public void setData(int x, int y) {
+    public SumClass(int x, int y) {
         a = x;
         b = y;
     }
 
-    public void calcData() {
-        int sum = a + b;
-        printMessage("Sum of two number is: " + sum);
-    }
-
-    public static void printMessage(String message) {
-        System.out.println(message);
+    public int calcData() {
+        return a + b;
     }
 }
-
-
